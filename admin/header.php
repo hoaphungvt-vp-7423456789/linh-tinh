dcff252d9ad37fcbaa542c73cd2a98fa6bfb6c91
hoa phung <hoaptph29645@fpt.edu.vn>
2023-03-08 17:42:15 +0700

    tất cả

---
+++ admin/header.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <div class="box-center">
+        <div class="row mb header">
+            <h1 class="quantri">ADMIN</h1>
+        </div>
+        <div class="row mb menu">
+            <ul>
+                <li>
+                    <a href="admin.html">Trang Chủ</a>
+                </li>
+
+                <li>
+                    <a href="">Danh Mục</a>
+                </li>
+
+                <li>
+                    <a href="">Hàng Hóa</a>
+                </li>
+
+                <li>
+                    <a href="">Khách Hàng</a>
+                </li>
+
+                <li>
+                    <a href="">Bình Luận</a>
+                </li>
+                
+                <li>
+                    <a href="">Thống Kê</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+    <div class="boxcenter">
+        <div class="row mb header">
+            <h1>ADMIN</h1>
+        </div>
+        <div class="row mb menu">
+            <ul>
+                <li>
+                    <a href="">Trang chủ</a>
+                </li>
+                <li>
+                    <a href="">Trang chủ</a>
+                </li>
+                <li>
+                    <a href="">Trang chủ</a>
+
+                </li>
+                <li>
+                    <a href="">Trang chủ</a>
+                </li>
+                <li>
+                    <a href="">Trang chủ</a>
+                </li>
+                <li>
+                    <a href="">Trang chủ</a>
+                </li>
+            </ul>
+        </div> -->
---
+++ admin/home.php
@@ -0,0 +1,38 @@
+<div class="row">
+    <div class="row-title">
+        <h1>THÊM MỚI LOẠI HÀNG HÓA</h1>
+    <div class="row-content">
+        <form action="" method="POST">
+            <div class="row mb10">
+                Mã loại <br>
+                <input type="text" name="maloai" id="" disabled>
+            </div>
+
+            <div class="row mb10">
+                Tên loại <br>
+                <input type="text" name="tenloai" id="">
+            </div>
+            
+            <div class="row mb10">
+                <input type="submit" name="" id="" value="THÊM MỚI">
+                <input type="reset" name="" id="" value="NHẬP LẠI">
+                <a href="ds_loaihang.html">
+                    <input type="button" value="DANH SÁCH">
+                </a>
+            </div>
+        </form>
+    </div>
+</div>
+
+<!-- <div class="row mb">
+            <div class="boxleft mr demo">
+
+            </div>
+            <div class="boxright demo">
+
+            </div>
+        </div>
+        <div class="row mb footer">
+
+        </div>
+    </div> -->
